42263f5fa34dc77f2424f54e02abf07879f24ce1
Michał Chmiel <mchmiel07@gmail.com>
2023-11-25 23:48:47 +0100

    Final fix for configuration

--- app/src/main/java/com/example/vindecoder/MainActivity.kt
+++ app/src/main/java/com/example/vindecoder/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.vindecoder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.vindecoder.ui.theme.VINDECODERTheme
 
 
@@ -17,20 +24,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Homepage()
+            VINDECODERTheme {
+
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen()
+        }
+//        composable("decoder") {
+//            ChatScreen()
+//        }
+//        composable("highlight") {
+//            SettingsScreen()
+//        }
+    }
 }
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun DefaultPreview() {
-    VINDECODERTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Home screen")
     }
 }
