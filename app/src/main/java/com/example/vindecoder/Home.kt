eae76964dccfeaedd316ef1be21828641f1bc44c
Michał Chmiel <mchmiel07@gmail.com>
2023-11-26 20:00:14 +0100

    Init commit for home screne content

--- app/src/main/java/com/example/vindecoder/Home.kt
+++ app/src/main/java/com/example/vindecoder/Home.kt
@@ -1,4 +1,5 @@
 package com.example.vindecoder
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,12 +8,17 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun HomeScreen() {
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+            .padding(30.dp)
     ) {
         Text(text = "Home screen")
     }
