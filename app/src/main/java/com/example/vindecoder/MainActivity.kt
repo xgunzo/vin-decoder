3c02dd264df76148b9bcbe4dd1e92a6c6a83b8f1
Michał Chmiel <mchmiel07@gmail.com>
2023-11-26 13:10:55 +0100

    Added functional navigation

--- app/src/main/java/com/example/vindecoder/MainActivity.kt
+++ app/src/main/java/com/example/vindecoder/MainActivity.kt
@@ -1,34 +1,84 @@
 package com.example.vindecoder
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Badge
+import androidx.compose.material.BadgedBox
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.vindecoder.ui.theme.VINDECODERTheme
 
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VINDECODERTheme {
-
+                val navController = rememberNavController()
+                Scaffold (
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Decoder",
+                                    route = "decoder",
+                                    icon = Icons.Default.Notifications
+                                ),
+                                BottomNavItem(
+                                    name = "Highlighted",
+                                    route = "highlighted",
+                                    icon = Icons.Default.Settings
+                                ),
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                ){
+                        Navigation(navController = navController)
+                    }
+                }
             }
         }
-    }
 }
 
 @Composable
@@ -37,16 +87,72 @@ fun Navigation(navController: NavHostController) {
         composable("home") {
             HomeScreen()
         }
-//        composable("decoder") {
-//            ChatScreen()
-//        }
-//        composable("highlight") {
-//            SettingsScreen()
-//        }
+        composable("decoder") {
+            DecoderScreen()
+        }
+        composable("highlighted") {
+            HighlightedScreen()
+        }
     }
 }
 
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
 
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(
+                        horizontalAlignment = CenterHorizontally
+                    ) {
+                        if(item.badgeCount > 0) {
+                            BadgedBox(
+                                badge = {
+                                   Badge {
+                                       Text(item.badgeCount.toString())
+                                   }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                            }
+                        } else {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+                        }
+                        if(selected) {
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun HomeScreen() {
@@ -54,5 +160,23 @@ fun HomeScreen() {
         modifier = Modifier.fillMaxSize()
     ) {
         Text(text = "Home screen")
+    }
+}
+
+@Composable
+fun DecoderScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Decoder screen")
+    }
+}
+
+@Composable
+fun HighlightedScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Highlighted screen")
     }
 }
