076993f2f8ad6f9efb8682646ca04aec07964827
Michał Chmiel <mchmiel07@gmail.com>
2023-11-28 20:26:48 +0100

    Added content and styles for home screen

--- app/src/main/java/com/example/vindecoder/Highlighted.kt
+++ app/src/main/java/com/example/vindecoder/Highlighted.kt
@@ -1,13 +1,17 @@
 package com.example.vindecoder
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
